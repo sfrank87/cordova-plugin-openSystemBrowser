a5c2fade7b182b6ca7d295cfa0f6c9693cabedc1
Frank Stingl <stingl.frank@inside-online.de>
2016-11-02 18:57:17 +0100

    clean up

--- src/android/opensystembrowser.java
+++ src/android/opensystembrowser.java
@@ -2,60 +2,22 @@
 package de.inside.cordova.opensystembrowser;
 
 import android.annotation.SuppressLint;
-//import android.content.Context;
 import android.content.Intent;
 import android.provider.Browser;
-//import android.content.res.Resources;
-//import android.graphics.Bitmap;
-//import android.graphics.drawable.Drawable;
 import android.net.Uri;
-// import android.os.Build;
-// import android.os.Bundle;
-// import android.text.InputType;
-// import android.util.TypedValue;
-// import android.view.Gravity;
-// import android.view.KeyEvent;
-// import android.view.View;
-// import android.view.Window;
-// import android.view.WindowManager;
-// import android.view.WindowManager.LayoutParams;
-// import android.view.inputmethod.EditorInfo;
-// import android.view.inputmethod.InputMethodManager;
-// import android.webkit.CookieManager;
-// import android.webkit.CookieSyncManager;
-// import android.webkit.HttpAuthHandler;
-// import android.webkit.WebSettings;
-// import android.webkit.WebView;
-// import android.webkit.WebViewClient;
-// import android.widget.EditText;
-// import android.widget.ImageButton;
-// import android.widget.ImageView;
-// import android.widget.LinearLayout;
-// import android.widget.RelativeLayout;
 
 import org.apache.cordova.CallbackContext;
-//import org.apache.cordova.Config;
 import org.apache.cordova.CordovaArgs;
-//import org.apache.cordova.CordovaHttpAuthHandler;
 import org.apache.cordova.CordovaPlugin;
-//import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.LOG;
-//import org.apache.cordova.PluginManager;
 import org.apache.cordova.PluginResult;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-//import java.lang.reflect.InvocationTargetException;
-// import java.lang.reflect.Field;
-// import java.lang.reflect.Method;
-// import java.util.HashMap;
-// import java.util.StringTokenizer;
 
 @SuppressLint("SetJavaScriptEnabled")
 public class opensystembrowser extends CordovaPlugin {
 
     protected static final String LOG_TAG = "opensystembrowser";
-    //private CallbackContext callbackContext;
 
     /**
      * Executes the request and returns PluginResult.
@@ -67,7 +29,6 @@ public class opensystembrowser extends CordovaPlugin {
      */
     public boolean execute(String action, CordovaArgs args, final CallbackContext callbackContext) throws JSONException {
         if (action.equals("open")) {
-            //this.callbackContext = callbackContext;
             final String url = args.getString(0);
 
             this.cordova.getActivity().runOnUiThread(new Runnable() {
@@ -99,7 +60,6 @@ public class opensystembrowser extends CordovaPlugin {
             // Adding the MIME type to http: URLs causes them to not be handled by the downloader.
             Uri uri = Uri.parse(url);
             if (!"file".equals(uri.getScheme())) {
-                //intent.setDataAndType(uri, webView.getResourceApi().getMimeType(uri));
                 intent.setData(uri);
                 intent.putExtra(Browser.EXTRA_APPLICATION_ID, cordova.getActivity().getPackageName());
                 this.cordova.getActivity().startActivity(intent);
